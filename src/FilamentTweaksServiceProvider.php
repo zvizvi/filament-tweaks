89d3728fe61ce23910d351a3bbc3add9e67111c9
zvizvi <5633910@gmail.com>
2025-08-13 19:11:59 +0300

    Update autogrow macro to accept maxHeight

--- src/FilamentTweaksServiceProvider.php
+++ src/FilamentTweaksServiceProvider.php
@@ -203,8 +203,16 @@ class FilamentTweaksServiceProvider extends PackageServiceProvider
         }
 
         if (config('filament-tweaks.features.enable_autogrow_textarea', true)) {
-            Textarea::macro('autogrow', function (): Textarea {
-                return $this->extraInputAttributes(['class' => 'autogrow']);
+            Textarea::macro('autogrow', function ($maxHeight = null): Textarea {
+                /**
+                 * @var Textarea $this
+                 */
+                $attributes = ['class' => 'autogrow'];
+                if ($maxHeight) {
+                    $attributes['style'] = 'max-height:'.$maxHeight;
+                }
+
+                return $this->extraInputAttributes($attributes);
             });
         }
 
